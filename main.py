d2dd03af6e89a8ffc6f6b8d339c15388f466696d
Mike <michael.hallam3@gmail.com>
2021-06-28 22:55:27 +0100

    implemented parser for file of products

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+import os
+PROD_FILE_NAME = 'products.json'
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    link, price = get_product_params()
+    wait_for_price(link, price)
+    print('bought products')
 
 
-# Press the green button in the gutter to run the script.
+# TODO - allow for multiple params at some point
+def get_product_params():
+    # we assume the input file is in json
+    import json
+    file = open(PROD_FILE_NAME, 'r')
+    products = json.loads(file.read())
+    file.close()
+    return products
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
